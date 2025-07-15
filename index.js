3e794ba85678329dbf57f518f135cea69a750241
Ansorn07 <babjirakhi123@gmail.com>
2025-07-15 12:47:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+
+require('dotenv').config()
+app.use(express.json())
+
+PORT = process.env.PORT || 4000;
+
+const fileupload = require('express-fileupload')
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}))
+
+
+// db cnection
+const db = require('./config/database')
+db.dbConnect();
+
+const cloudinary = require('./config/cloudinary')
+cloudinary.cloudinaryConnect();
+
+// api routes mount karna h
+const Upload = require('./routes/FileUpload')
+app.use('/api/v1/upload', Upload)
+
+// activate server
+app.listen(PORT, () => {
+    console.log(`Server is started at ${PORT}...`);
+})
+
+
